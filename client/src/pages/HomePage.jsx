5e37564497267e41679ac89c072512ec3cfd3ae5
trainthunder <kitikun.po@gmail.com>
2024-06-13 14:22:09 +0700

    feat: TrainHackhour

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,29 +1,8 @@
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import useBlogPost from "../hooks/useBlogPosts";
+
 
 function HomePage() {
-  const navigate = useNavigate();
-
-  const [posts, setPosts] = useState([]);
-  const [isError, setIsError] = useState(null);
-  const [isLoading, setIsLoading] = useState(null);
-
-  const getPosts = async () => {
-    try {
-      setIsError(false);
-      setIsLoading(true);
-      const results = await axios("http://localhost:4000/posts");
-      setPosts(results.data.data);
-      setIsLoading(false);
-    } catch (error) {
-      setIsError(true);
-    }
-  };
-
-  useEffect(() => {
-    getPosts();
-  }, []);
+  const { navigate, posts, isError, isLoading } = useBlogPost();
 
   return (
     <div>
