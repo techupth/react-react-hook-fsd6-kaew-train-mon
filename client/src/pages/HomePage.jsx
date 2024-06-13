f55aed98e71161d68d2f07ba8df26cf198dd2699
Mon <tracer.coolkid@gmail.com>
2024-06-13 14:18:55 +0700

    feat: add hook useBlogPosts

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,30 +1,13 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import useBlogPosts from "../hook/useBlogPosts";
 
 function HomePage() {
   const navigate = useNavigate();
+  const { posts, isError, isLoading } = useBlogPosts();
 
-  const [posts, setPosts] = useState([]);
-  const [isError, setIsError] = useState(null);
-  const [isLoading, setIsLoading] = useState(null);
-
-  const getPosts = async () => {
-    try {
-      setIsError(false);
-      setIsLoading(true);
-      const results = await axios("http://localhost:4000/posts");
-      setPosts(results.data.data);
-      setIsLoading(false);
-    } catch (error) {
-      setIsError(true);
-    }
-  };
-
-  useEffect(() => {
-    getPosts();
-  }, []);
-
+  
   return (
     <div>
       <div className="app-wrapper">
