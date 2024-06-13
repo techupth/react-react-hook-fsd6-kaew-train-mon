9d722a7e80a1f30bcde379de7682d41551bfdcb1
Kanokphit <kanokphit55@gmail.com>
2024-06-13 14:08:21 +0700

    "React React Custom Hook Get Posts Assignment"

---
+++ client/src/hooks/useBlogPosts.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+function useBlogPosts() {
+  const [posts, setPosts] = useState([]);
+  const [isError, setIsError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const navigate = useNavigate();
+
+  const getPosts = async () => {
+    try {
+      setIsError(false);
+      setIsLoading(true);
+      const results = await axios("http://localhost:4000/posts");
+      setPosts(results.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsError(true);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+  return { posts, isError, isLoading, navigate };
+}
+
+export default useBlogPosts;
